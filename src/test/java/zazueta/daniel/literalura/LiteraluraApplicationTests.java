db77b2aa7eafb26b19c612ca429242c3a0ff1b62
Daniel Zazueta <dzazueta.solano@gmail.com>
2024-07-08 11:34:49 -0700

    Iniciando el repositorio, primer commit

---
+++ src/test/java/zazueta/daniel/literalura/LiteraluraApplicationTests.java
@@ -0,0 +1,13 @@
+package zazueta.daniel.literalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
